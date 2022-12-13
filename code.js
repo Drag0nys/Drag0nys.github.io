4b587a03c93b38dcfff451c0c08384ebeb7e6df6
Drag0nys <84237745+Drag0nys@users.noreply.github.com>
2022-12-13 21:35:55 +0100

    Update code.js

--- code.js
+++ code.js
@@ -8,12 +8,8 @@ function inscriptionPress(){
     var chapitre_ = chapitre.value;
     
     var liste = [prenom_, horaire_, chapitre_];
-    var final = liste.join(" / ");
+    var final = liste.join(" and ");
     var document.getElementById("rep").innerHTML = final;
-    
-    var document.getElementById("prenom").innerHTML = "";
-    var document.getElementById("horaire").innerHTML = "";
-    var document.getElementById("chapitre").innerHTML = "";
     
     console.log(liste)
     
