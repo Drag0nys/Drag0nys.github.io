a2a916e8a0a99d45821890aa143502a02a15dda4
Drag0nys <84237745+Drag0nys@users.noreply.github.com>
2022-12-14 15:55:25 +0100

    Update code.js

--- code.js
+++ code.js
@@ -2,12 +2,10 @@ function inscriptionPress(){
     var prenom = document.getElementById("prenom");
     var horaire = document.getElementById("horaire");
     var chapitre = document.getElementById("chapitre");
-    var donner = document.getElementById("donner");
     
     var prenom_ = prenom.value;
     var horaire_ = horaire.value;
     var chapitre_ = chapitre.value;
-    var donner_ = donner.value;
     
     let p1 = document.getElementById('p1');
     var liste = [prenom_, horaire_, chapitre_];
